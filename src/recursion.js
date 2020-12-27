32cbfa47c1aa17097e2f35ff69edc2f0aaf10c24
Steve <steve.gackstetter@gmail.com>
2020-12-27 09:58:16 -0600

    Completed reverse array

--- src/recursion.js
+++ src/recursion.js
@@ -296,6 +296,13 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+
+  var returnArr = [array[array.length - 1]];
+
+  return returnArr.concat(reverseArr(array.slice(0, -1)));
 };
 
 // 18. Create a new array with a given value and length.
